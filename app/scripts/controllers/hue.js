2970a34fca92af5a685d8d92d675b45bcade2bf8
Karthik Kotha <karthik@Karthiks-MacBook-Pro.local>
2015-09-16 20:49:01 -0500

    functioning lights

--- app/scripts/controllers/hue.js
+++ app/scripts/controllers/hue.js
@@ -11,6 +11,10 @@ angular.module('iotAppApp')
     myHue.setup({username: 'newdeveloper', bridgeIP: '10.0.1.2', debug: true});
 
     function turnOffLights() {
+      myHue.setGroupState(1, {on: false});
+      myHue.setGroupState(2, {on: false});
+      myHue.setGroupState(3, {on: false});
+
       myHue.setLightState(4, {"on": false});
     }
 
@@ -21,6 +25,55 @@ angular.module('iotAppApp')
     function init() {
       vm.turnOffLights = turnOffLights;
       vm.turnOnLights = turnOnLights;
+      vm.setDefaultScene = setDefaultScene;
+      vm.setMovieMode = setMovieMode;
+      vm.setVirginAtlanticPurple = setVirginAtlanticPurple;
+      vm.setHalloween = setHalloween;
+      vm.setDeepBlue = setDeepBlue;
+      vm.setTVColorLoop = setTVColorLoop;
     }
+
+    function setDefaultScene() {
+      myHue.setGroupState(1, {on: true, "scene": "e02612416-on-0"});
+      myHue.setGroupState(2, {on: true, "scene": "e02612416-on-0"});
+      myHue.setGroupState(3, {on: true, "scene": "e02612416-on-0"});
+
+      myHue.setLightState(4, {"on": true, "scene": "e02612416-on-0"});
+    }
+
+    function setMovieMode() {
+      var transitionTime = 6;
+      var brightness = 40;
+      myHue.setGroupState(1, {on: true, scene: "3b727eaba-on-0", bri: brightness, transitiontime: transitionTime});
+      myHue.setGroupState(2, {on: true, scene: "3b727eaba-on-0", bri: brightness, transitiontime: transitionTime});
+      myHue.setGroupState(3, {on: false, bri: brightness, transitiontime: transitionTime});
+    }
+
+    function setVirginAtlanticPurple() {
+      myHue.setGroupState(1, {on: true, "scene": "e210eae61-on-0"});
+      myHue.setGroupState(2, {on: true, "scene": "e210eae61-on-0"});
+      myHue.setGroupState(3, {on: true, "scene": "e210eae61-on-0"});
+    }
+
+    function setHalloween() {
+      myHue.setGroupState(1, {on: true, "scene": "496099b7c-on-0"});
+      myHue.setGroupState(2, {on: true, "scene": "496099b7c-on-0"});
+      myHue.setGroupState(3, {on: true, "scene": "496099b7c-on-0"});
+    }
+
+    function setDeepBlue() {
+      myHue.setGroupState(1, {on: true, "scene": "121fa9f80-on-0"});
+      myHue.setGroupState(2, {on: true, "scene": "121fa9f80-on-0"});
+      myHue.setGroupState(3, {on: true, "scene": "121fa9f80-on-0"});
+    }
+
+    function setTVColorLoop(){
+      myHue.setGroupState(1, {on: true, bri: 255, transitiontime: 10, effect: "colorloop"});
+      myHue.setGroupState(2, {on: true, bri: 255, scene: "496099b7c-on-0"});
+      myHue.setGroupState(3, {on: true, bri: 255, scene: "496099b7c-on-0"});
+
+      myHue.setLightState(4, {on: true, bri: 255, transitiontime: 10, effect: "colorloop"});
+    }
+
     init();
   });
